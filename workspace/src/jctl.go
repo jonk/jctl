a21a5313bddc6dade3d6e48753d93c073265d16e
Jon Kalfayan <jonkalfayan@gmail.com>
2018-08-25 13:50:57 -0700

    initial commitl, go code and notes

---
+++ workspace/src/jctl.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+    "fmt"
+    "flag"
+    "context"
+    "log"
+
+    "github.com/ericchiang/k8s"
+    corev1 "github.com/ericchiang/k8s/apis/core/v1"
+)
+
+func main() {
+    flag.Parse()
+    
+    client, err := k8s.NewInClusterClient()
+    if err != nil {
+        log.Fatal(err)
+    }
+    var nodes corev1.NodeList
+    if err := client.List(context.Background(), "", &nodes); err != nil {
+        log.Fatal(err)
+    }
+
+    for _, node := range nodes.Items {
+        fmt.Printf("name=%q schedulable=%t\n", *node.Metadata.Name, !*node.Spec.Unschedulable)
+    }
+}
